e2d6c2c7c38e2b3e21d654c5ee93481b3a20ef66
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-09 17:32:11 -0400

    Creados JSON de idioma, implementada traducción en componente

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,3 +1,4 @@
+import { TranslateService } from '@ngx-translate/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  user!: {
+    firstName: string;
+    lastName: string;
+  }
+
+  welcome!: string;
+
+  usernameLabel!: string;
+  passwordLabel!: string;
+
+  constructor(
+    private translate: TranslateService
+  ) { }
 
   ngOnInit(): void {
+    this.user = {
+      firstName: 'John',
+      lastName:'Doe'
+    };
+
+    this.welcome = this.translate.instant('welcomeMessage', {
+      firstName: this.user.firstName, lastName: this.user.lastName
+    });
+
+    this.translate.get(['login.username', 'login.password']).subscribe(translations => {
+      this.usernameLabel = translations['login.username'];
+      this.passwordLabel = translations['login.password'];
+    });
+
   }
 
 }
